d5df48f51453c6851aadaf844dae4df96fa9f9b8
git practice <abhijith7133@gmail.com>
2025-09-14 15:58:43 +0530

    completed

---
+++ index.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import { connectDB } from './lib/db.js'; 
+import cors from 'cors';
+import { app, server } from './lib/socket.js';  
+
+dotenv.config();
+
+import authRoutes from './routes/authRoutes.js';  
+import messageRoutes from './routes/messageRoutes.js';  
+
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
+app.use(cookieParser());
+
+const PORT = process.env.PORT || 5001;
+
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  })
+);
+
+app.use('/api/auth', authRoutes);
+app.use('/api/message', messageRoutes);
+
+connectDB()
+  .then(() => {
+    server.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error('Failed to connect to DB:', err);
+  });
